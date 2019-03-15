c3c2f33969e52aba2651a7224a1ad4ab9c24ffdd
samjeffcoat <samjeffcoat@gmail.com>
2019-03-15 10:26:28 -0500

    Creates games array to use

---
+++ server.js
@@ -0,0 +1,38 @@
+const games = [
+    {
+        id: 0,
+        title: 'Pacman',
+       genre: 'Arcade',
+        releaseYear: 1980
+    },
+    {
+        id: 1,
+        title: 'Super Mario Brothers',
+        genre: 'Arcade',
+        releaseYear: 1985
+    },
+    {
+        id: 3,
+        title: 'Madden 2019',
+        genre: 'Sports',
+        releaseYear: 2019
+    },
+    {
+        id: 4,
+        title: 'MLB the Show',
+        genre: 'Sports',
+        releaseYear: 2019
+    },
+    {
+        id: 5,
+        title: 'NCAA Football 2015',
+        genre: 'Sports',
+        releaseYear: 2015
+    },
+    {
+        id: 6,
+        title: 'FIFA 2019',
+        genre: 'Sports',
+        releaseYear: 2019
+    },
+];
